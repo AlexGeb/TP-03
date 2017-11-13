cc3514940fb46a41019f7be5a6c2bf71b1ed5c75
AlexGeb <alex.behaghel@gmail.com>
2017-11-13 10:44:40 +0100

    traduction

--- src/exo5/TestFibonacci.java
+++ src/exo5/TestFibonacci.java
@@ -14,13 +14,14 @@ public class TestFibonacci {
 		long start2 = System.nanoTime();
 		double N2 = fibonacci2(N);
 		long elapsedTime2 = System.nanoTime() - start2;
-		System.out.println("Seconde méthode : \n" + N2 + "\n calculé en " + elapsedTime2 * 10E-6 + " ms");
+		System.out.println(
+				"Method with a power matrix : \n" + N2 + "\n calcul duration : " + elapsedTime2 * 10E-6 + " ms");
 
 		// Recursive method
 		long start = System.nanoTime();
 		double N1 = fibonacci(N);
 		long elapsedTime = System.nanoTime() - start;
-		System.out.println("Méthode récursive : \n" + N1 + "\n calculé en " + elapsedTime * 10E-6 + " ms");
+		System.out.println("Recursive method : \n" + N1 + "\n calcul duration : " + elapsedTime * 10E-6 + " ms");
 
 		// close the scanner object
 		questionUser.close();
@@ -52,9 +53,5 @@ public class TestFibonacci {
 				for (int k = 0; k < 2; ++k)
 					result[i][j] += a[i][k] * b[k][j];
 		return result;
-	}
-
-	private static String matrixToString(double[][] a) {
-		return a[0][0] + " " + a[0][1] + "\n" + a[1][0] + " " + a[1][1];
 	}
 }
